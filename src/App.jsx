a249c7fbbc7b27b093af475354a9c08ed5b67289
revv2703 <asijaprakash27@gmail.com>
2023-11-04 19:12:57 +0530

    added star animation from the about section

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
         <div className='relative z-0'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
           <Contact />
           <StarsCanvas />
         </div>
